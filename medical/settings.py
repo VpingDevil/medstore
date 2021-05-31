73c98eeff3d75f1d478e53f5e803b27c9eb41dd6
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2021-05-31 20:52:39 +0530

    configured heroku

--- medical/settings.py
+++ medical/settings.py
@@ -146,6 +146,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 SITE_ID = 1
 LOGIN_REDIRECT_URL = "/"
